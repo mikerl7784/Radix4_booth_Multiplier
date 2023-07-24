4c3085cdb40e34cff8837d52d912742147423c08
Yu_Lin_Liu <mikerl7784@gmail.com>
2023-07-24 15:43:19 +0800

    first commit

---
+++ Full_Adder.v
@@ -0,0 +1,12 @@
+//-----------------------------------------------------------------------------
+// 1-bit full_adde
+//-----------------------------------------------------------------------------
+
+
+module full_adder(in0, in1, cin, out, cout);
+input in0, in1, cin;
+output out, cout;
+
+assign out = in0 ^ in1 ^ cin;
+assign cout = ((in0 ^ in1) & cin) | (in0 & in1);
+endmodule
